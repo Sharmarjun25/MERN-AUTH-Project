649e3aba617462112a8ac8d8cffbeb23f04411db
unknown <arjun2508sharma@gmail.com>
2026-02-19 00:36:53 +0530

    updated code for auth

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,35 +1,102 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { useState } from 'react';
+import { handleError } from '../utils';
+import { useNavigate } from 'react-router-dom';
 
 function Signup() {
+
+    const [signupInfo, setSignupInfo] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        console.log(name, value);
+        const copySignupInfo = { ...signupInfo };
+        copySignupInfo[name] = value;
+        setSignupInfo(copySignupInfo);
+    };
+    console.log('signupInfo -> ', signupInfo);
+    const handleSignup = async (e) => {
+        e.preventDefault();
+        const { name, email, password } = signupInfo;
+        /*if (!name || !email || !password) {
+            return handleError('All fields are required')
+        }*/
+        if (!name) {
+            return handleError('Name is required')
+        }
+        if (!email) {
+            return handleError('Email is required')
+        }
+        if (!password) {
+            return handleError('Password is required')
+        }
+        try {
+
+            const url = "http://localhost:8080/auth/signup";
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(signupInfo)
+            });
+            const result = await response.json();
+            const { success, message } = result;
+            if (success) {
+                handleSuccess(message);
+                setTimeout(() => {
+                    navigate('/login')
+                }, 1000)
+            }
+
+            console.log(result);
+        } catch (error) {
+            handleError(error);
+        }
+
+    }
+
     return (
         <div className='container'>
             <h1>Signup</h1>
-            <form>
+            <form onSubmit={handleSignup}>
                 <div>
                     <label htmlFor='name'>Name</label>
                     <input
+                        onChange={handleChange}
                         type="text"
                         name='name'
                         autoFocus
                         placeholder='Enter your name...'
+                        value={signupInfo.name}
                     />
                 </div>
                 <div>
                     <label htmlFor='email'>Email</label>
                     <input
+                        onChange={handleChange}
                         type="email"
                         name='email'
                         placeholder='Enter your email...'
+                        value={signupInfo.email}
                     />
                 </div>
                 <div>
                     <label htmlFor='password'>Password</label>
                     <input
+                        onChange={handleChange}
                         type="password"
                         name='password'
                         placeholder='Enter your password...'
+                        value={signupInfo.password}
                     />
                 </div>
                 <button type="submit">Signup</button>
